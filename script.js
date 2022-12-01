f250730d8b7cb39df74f79949c1ba784d1d87d92
Tevikansky <valerabogdanov@inbox.ru>
2022-12-01 14:00:11 +0600

    моё первое задание

---
+++ script.js
@@ -0,0 +1,8 @@
+let title = 1;
+let screens = 2;
+let screenPrice = 3;
+let rollback = 4;
+let fullPrice = 5;
+let adaptive = 6;
+alert("Hello my friend!")
+console.log("Try to find text in terminal");
