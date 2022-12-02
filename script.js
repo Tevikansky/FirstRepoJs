af4eab860a765c62997904ea0b14a6275f7d45b1
Tevikansky <valerabogdanov@inbox.ru>
2022-12-02 18:53:46 +0600

    выполнено задание 1 в уроке 2

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let title = 1;
-let screens = 2;
-let screenPrice = 3;
-let rollback = 4;
-let fullPrice = 5;
-let adaptive = 6;
-alert("Hello my friend!")
-console.log("Try to find text in terminal");
+let title = "First project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 69;
+let rollback = 76;
+let fullPrice = 45000;
+let adaptive = false;
+
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов', screenPrice, 'рублей/долларов/гривен/юани');
+console.log('Стоимость разработки сайта', fullPrice, 'рублей/долларов/гривен/юани');
+console.log(screens.toLowerCase().split());
+console.log('Процент отката посреднику за работу', fullPrice * (rollback / 100));
