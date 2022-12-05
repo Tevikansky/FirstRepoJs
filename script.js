0aeba89e69d2422778eeab57a8d61a8edecd8b48
Tevikansky <valerabogdanov@inbox.ru>
2022-12-06 00:35:19 +0600

    выполненно задание урока №3

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ console.log('Стоимость верстки экранов', screenPrice, 'р
 console.log('Стоимость разработки сайта', fullPrice, 'рублей/долларов/гривен/юани');
 console.log(screens.toLowerCase().split());
 console.log('Процент отката посреднику за работу', fullPrice * (rollback / 100));
+
+title = prompt("Как называется ваш проект?");
+screens = prompt("Какие типы экранов нужно разработать?");
+screenPrice = Number(prompt("Сколько будет стоить данная работа?"));
+adaptive = prompt('Нужен ли адаптив на сайте?').toLowerCase().replace(/[^a-zа-яё\s]/gi, '').trim();
+adaptive = adaptive == "да" ? true : false;
+service1 = prompt("Какой дополнительный тип услуги нужен?");
+servicePrice1 = Number(prompt('Сколько это будет стоить?'));
+service2 = prompt("Какой дополнительный тип услуги нужен?");
+servicePrice2 = Number(prompt('Сколько это будет стоить?'));
+fullPrice = screenPrice + servicePrice1 + servicePrice2;
+servicePercentPrice = Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+console.log(servicePercentPrice);
+switch (true) {
+  case fullPrice >= 30000:
+    console.log("Даем скидку в 10%");
+    break;
+  case fullPrice >= 15000 && fullPrice < 30000:
+    console.log("Даем скидку в 5%");
+    break;
+  case fullPrice < 15000 && fullPrice >= 0:
+    console.log("Скидка не предусмотрена");
+    break;
+  default:
+    console.log("Что то пошло не так");
+}
