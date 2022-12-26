72c2ff126f0b75293f4e994a072b967b3263b799
Tevikansky <valerabogdanov@inbox.ru>
2022-12-27 00:58:33 +0600

    исправлена ошибка в 14 уроке

--- js/script.js
+++ js/script.js
@@ -165,7 +165,7 @@ const appData = {
   addScreenBlock: function () {
     screens = document.querySelectorAll('.screen');
     const cloneScreen = screens[0].cloneNode(true);
-
+    cloneScreen.querySelector('input[type=text]').value = '';
     screens[screens.length - 1].after(cloneScreen);
     appData.screens.splice(0);
   },
